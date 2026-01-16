59c0c66bd6a67f859bbd47b104858d4b9da95335
ozdildanya <ozdil.danya@gmail.com>
2026-01-17 00:06:12 +0300

    Updated security.py for new demo user

--- backend/backend/app/core/security.py
+++ backend/backend/app/core/security.py
@@ -2,8 +2,13 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# This one is for auth_service.py
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
+# This is an alias so main.py code also works
+def get_password_hash(password: str) -> str:
+    return hash_password(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
