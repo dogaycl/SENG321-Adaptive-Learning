16c95e691ef1682813b63b6cc46304b28bcbbf4a
lbusegurdal <sshbmvpzkr@privaterelay.appleid.com>
2026-01-16 17:19:35 +0300

    Update history.py

--- backend/backend/app/schemas/history.py
+++ backend/backend/app/schemas/history.py
@@ -1,17 +1,20 @@
-import datetime
-from pydantic import BaseModel, EmailStr
-
+# Dosya: app/schemas/history.py
 from pydantic import BaseModel
+from datetime import datetime
 
 class HistoryBase(BaseModel):
     question_id: int
     given_answer: str
-    is_correct: bool
     time_spent_seconds: int
+
+class HistoryCreate(HistoryBase):
+    # Service katmanında is_correct hesaplandığı için burada sadece girdi alıyoruz
+    pass
 
 class HistoryResponse(HistoryBase):
     id: int
     user_id: int
+    is_correct: bool
     solved_at: datetime
     class Config:
         from_attributes = True
